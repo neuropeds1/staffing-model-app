b277106da72334289175d7b07e3be545de5e726f
neuropeds1 <91512784+neuropeds1@users.noreply.github.com>
2025-07-24 07:38:17 -0700

    staffing_app.py

--- staffing_app.py
+++ staffing_app.py
@@ -97,7 +97,7 @@ elif clinician_type == "APP (Day & Night)":
 elif clinician_type == "UW Neurology R2":
     with st.form("uw_r2_form"):
         rotations = st.number_input("Number of 28-day rotations", min_value=1, step=1)
-        day_per_rot = 18  # TODO: update this
+        day_per_rot = 17  # TODO: update this
         night_per_rot = 5  # TODO: update this
         if st.form_submit_button("➕ Add to Model"):
             add_entry(clinician_type, day_per_rot * rotations, night_per_rot * rotations, {"Rotations": rotations})
@@ -105,7 +105,7 @@ elif clinician_type == "UW Neurology R2":
 elif clinician_type == "UW Neurology R3/4":
     with st.form("uw_r34_form"):
         rotations = st.number_input("Number of 28-day rotations", min_value=1, step=1)
-        day_per_rot = 18  # TODO: update this
+        day_per_rot = 17  # TODO: update this
         night_per_rot = 5  # TODO: update this
         if st.form_submit_button("➕ Add to Model"):
             add_entry(clinician_type, day_per_rot * rotations, night_per_rot * rotations, {"Rotations": rotations})
